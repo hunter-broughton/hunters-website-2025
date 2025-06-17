0e35c1f1111ab4668abc16a3ffac74c8eb1360d0
Hunter Broughton <huntbro@umich.edu>
2025-06-16 23:00:52 -0700

    Update deployment configuration for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
   images: {
     domains: ['firebasestorage.googleapis.com'],
+    unoptimized: true,
   },
+  basePath: '',
 }
 
 module.exports = nextConfig 
